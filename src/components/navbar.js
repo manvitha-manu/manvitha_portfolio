8ef609bc5f3747d31479e67e567f7313de723363
Manvitha_Golla <manvithagc@gmail.com>
2023-10-07 15:28:27 -0700

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import './navbar.css';
+import logo from '../assets/logo.png';
+import contactImg from '../assets/contact.png'
+import {Link} from 'react-scroll';
+
+const Navbar = () => {
+    return (
+            <nav className="navbar">
+                <img src={logo} alt="Logo" className="Logo"/>
+                <div className="desktopMenu">
+                    <Link className="desktopMenuListItem">Home</Link>
+                    <Link className="desktopMenuListItem">About</Link>
+                    <Link className="desktopMenuListItem">Portfolio</Link>
+                    </div>          
+            <button className="desktopMenu">   
+                <img src={contactImg} alt=""className="desktopMenuImg"/>Contact Me
+            </button>
+            </nav>
+            
+      );
+}
+ 
+export default Navbar;  
