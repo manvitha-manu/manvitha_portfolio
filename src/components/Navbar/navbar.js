423d2af0f76094695355568fdf1623e32af326ec
Manvitha_Golla <manvithagc@gmail.com>
2023-12-16 11:34:14 -0800

    changes

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import './navbar.css';
 import logo from '../../assets/logonobg.png';
-import contactImg from '../../assets/contact.png'
+import contactImg from '../../assets/contact.png';
 import {Link} from 'react-scroll';
 import menu from'../../assets/menu.png';
 const Navbar=()=>{
